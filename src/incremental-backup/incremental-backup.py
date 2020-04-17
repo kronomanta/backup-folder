d4c4e3a4a4b4f5c9b891df18019a7f416d26a36a
Tibor Kanyo <kronomanta@users.noreply.github.com>
2020-04-17 03:13:43 +0200

    Update incremental-backup.py
    shifting versions works now

--- src/incremental-backup/incremental-backup.py
+++ src/incremental-backup/incremental-backup.py
@@ -8,7 +8,7 @@ def parse_input():
   parser.add_argument("-s", "--source", help="source folder path", required=True)
   parser.add_argument("-t", "--destination", help="destination folder path", required=True)
   parser.add_argument("-r", "--rotation", help="max instance to keep", type=int, required=True)
-	
+
   return parser.parse_args()
   
 if __name__ == '__main__':
@@ -30,20 +30,21 @@ if __name__ == '__main__':
     #get all existing folder containing older versions
     all_versions = []
     for elem_name in os.listdir(destination_folder):
-      full_elem_path = os.path.join(destination_folder, elem_name)
-      if os.path.isdir(full_elem_path):
-        all_versions.append(full_elem_path)
+      if elem_name.isdigit():
+        full_elem_path = os.path.join(destination_folder, elem_name)
+        if os.path.isdir(full_elem_path):
+          all_versions.append(full_elem_path)
         
     
     version_count = len(all_versions)
     if (version_count == 0):
-      # create first backup folder
+      print("create first backup folder")
       new_version_path = os.path.join(destination_folder, "0") 
       os.makedirs(new_version_path)
       all_versions.append(new_version_path)
     elif (version_count < args.rotation):
-      # create a folder for older version
-      latest_version = int(os.path.dirname(all_versions[version_count-1]))
+      print("create a folder for older version")
+      latest_version = int(os.path.basename(all_versions[version_count-1]))
       new_version_path = os.path.join(destination_folder, str(latest_version + 1))
       os.makedirs(new_version_path)
       all_versions.append(new_version_path)
@@ -52,20 +53,24 @@ if __name__ == '__main__':
       for i in range(args.rotation, version_count):
         print("delete old version: " + all_versions[i])
         shutil.rmtree(all_versions[i])
+      all_versions = all_versions[: args.rotation]
        
     rsync_result = -1
     
     # iterate from the element before the last one to the first one, so we shift the backups one day older
-    for i in range(len(all_versions) - 2, 0):
-      print(all_versions[i])
-      rsync_result = os.system("rsync -avhW {} {} > {} 2>{}".format(all_versions[i - 1], all_versions[i], log_file, log_file))
-      if rsync_result != 0:
-        raise Exception("rsync failed: {}, source: {}, target: {}".format(str(rsync_result), all_versions[i - 1], all_versions[i]))
+    print("make previous backups older")
+    if len(all_versions) > 1:
+      for i in range(len(all_versions) - 2, -1, -1):
+        print(all_versions[i] + " -> "+all_versions[i+1])
+        rsync_result = os.system(r"rsync -avh --delete {}/ {}/ > {} 2>{}".format(all_versions[i], all_versions[i+1], log_file, log_file))
+        if rsync_result != 0:
+          raise Exception("rsync failed: {}, source: {}, target: {}".format(str(rsync_result), all_versions[i], all_versions[i+1]))
     
     # backup live data
-    rsync_result = os.system("rsync -avhW {} {} > {} 2>{}".format(args.source, all_versions[0], log_file, log_file))
+    print("backup live data")
+    rsync_result = os.system(r"rsync -avh --delete {}/ {} > {} 2>{}".format(args.source.rstrip("/"), all_versions[0], log_file, log_file))
     if rsync_result != 0:
-      raise Exception("rsync failed: {}, source: {}, target: {}".format(str(rsync_result), args.source, all_versions[0]))
+      raise Exception("rsync failed: {}, source: {}, target: {}".format(str(rsync_result), args.source.rstrip("/"), all_versions[0]))
     
     
     print('### Finished successfully ###')	

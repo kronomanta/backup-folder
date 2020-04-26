94b30b149f51d939d04bae4164457b5a2a263867
Kanyo Tibor <kanyo.tibi@gmail.com>
2020-04-27 00:30:41 +0200

    logging added

--- src/incremental-backup/incremental-backup.py
+++ src/incremental-backup/incremental-backup.py
@@ -5,7 +5,9 @@ import shutil
 import traceback
 import sys
 
-__version_info__ = (1, 0, 0)
+import logging
+
+__version_info__ = (1, 1, 0)
 __version__ = '.'.join(str(c) for c in __version_info__)
 
 
@@ -32,12 +34,12 @@ class BackupManager:
   def handleVersionFolders(self, all_versions):
     version_count = len(all_versions)
     if (version_count == 0):
-      print("create first backup folder", flush=True)
+      logging.info("create first backup folder")
       new_version_path = os.path.join(self.destination_folder, "0") 
       os.makedirs(new_version_path)
       all_versions.append(new_version_path)
     elif (version_count < self.rotation):
-      print("create a folder for older version", flush=True)
+      logging.info("create a folder for older version")
       latest_version = int(os.path.basename(all_versions[version_count-1]))
       new_version_path = os.path.join(self.destination_folder, str(latest_version + 1))
       os.makedirs(new_version_path)
@@ -45,31 +47,31 @@ class BackupManager:
     else:
       # delete too old versions
       for i in range(self.rotation, version_count):
-        print("delete old version: " + all_versions[i], flush=True)
+        logging.info("delete old version: " + all_versions[i])
         shutil.rmtree(all_versions[i])
       all_versions = all_versions[: self.rotation]  
     return all_versions
 
   # iterate from the element before the last one to the first one, so we shift the backups one day older
   def shiftVersions(self, all_versions):
-    print("make previous backups older", flush=True)
+    logging.info("make previous backups older")
     if len(all_versions) > 1:
       for i in range(len(all_versions) - 2, -1, -1):
-        print("{} -> {}".format(all_versions[i], all_versions[i+1]), flush=True, end="\n\n")
+        logging.info("{} -> {}\n\n".format(all_versions[i], all_versions[i+1]))
         rsync_result = subprocess.run(["rsync", "-avh", "--delete", all_versions[i] + "/", all_versions[i+1] + "/"])
         if rsync_result.returncode != 0:
           raise Exception("exit result: {}".format(rsync_result))
   
   # backup live data
   def backupLiveData(self, target_folder):
-    print("backup live data: {} -> {}".format(self.source, target_folder), flush=True, end="\n\n")
+    logging.info("backup live data: {} -> {}\n\n".format(self.source, target_folder))
     rsync_result = subprocess.run(["rsync", "-avh", "--delete", self.source + "/", target_folder + "/"])
     if rsync_result.returncode != 0:
       raise Exception(" exit result {}".format(rsync_result))
 
 	
   def backup(self):
-    print('### Start backup of ' + self.source +' ####', flush=True)
+    logging.info('### Start backup of ' + self.source +' ####')
     
     #get all existing folder containing older versions
     all_versions = self.handleVersionFolders(self.getVersions())
@@ -88,11 +90,16 @@ def parse_input():
   
 if __name__ == '__main__':
   try:  
+    logging.basicConfig(
+      format='%(asctime)s %(levelname)-8s %(message)s',
+      level=logging.INFO,
+      datefmt='%Y.%m.%d %H:%M:%S')
+
     args = parse_input()
 
     BackupManager(args).backup()
-    print('### Finished successfully ###', flush=True)
+    logging.info('### Finished successfully ###')
   except: # catch *all* exceptions
-    print('### ERROR ###', flush=True)
-    print('Directory not copied. Error: {}'.format(traceback.format_exc()), flush=True)
+    logging.error('### ERROR ###')
+    logging.error('Directory not copied. Error: {}'.format(traceback.format_exc()))
     sys.exit(1)
